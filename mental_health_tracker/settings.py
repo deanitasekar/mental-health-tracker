a0659451424ef912ea371bffe80ef8d47a3f6304
deanitasekar <deanitasekar@gmail.com>
2024-09-16 19:49:55 +0700

    update settings.py

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t)s=-s+7$ot3b97mk&li+#b&+y9gdhu!32sn#i00m3f3wk)f3%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "deanita-sekar-mentalhealthtracker1.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "deanita-sekar-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definitionn
